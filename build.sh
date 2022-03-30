a3cf08fceae201775e3e8956d17481ae2d4bdd26
c3eru <yyprjkt@gmail.com>
2022-03-30 20:38:05 +0700

    Test

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/c3eru/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=mobxprjkt
 export BUILD_HOSTNAME=RANDOMBUILD
-lunch cherish_juice-userdebug
+lunch aosp_juice-userdebug
 
-brunch juice
+mka bacon
 
 # end
