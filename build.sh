801e559009ca6f6107ca2f190510e7355c6da8f4
c3eru <yyprjkt@gmail.com>
2022-03-30 10:41:01 +0700

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest --depth 1 -b CherishOS .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta
+export BUILD_USERNAME=mobxprjkt
+export BUILD_HOSTNAME=RANDOMBUILD
+lunch cherish_juice-userdebug
+
+brunch juice -j8
+
+# end
